fbe1ff4f9dfdf0bb83f69013a79f5f73aa887681
Giga Razki Arianda <razkigiga@email.com>
2024-05-20 18:32:35 +0700

    Email-Spam-Classifier V.1

--- input-data.py
+++ input-data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-# Membuat dataset contoh
+
 data = {
     'email': [
         "Congratulations! You've won a free lottery ticket!",
@@ -20,18 +20,17 @@ data = {
     ]
 }
 
-# Mengubah dictionary menjadi DataFrame
+
 df = pd.DataFrame(data)
 
-# Menentukan path folder dan file
+
 folder_path = 'dataset'
 file_path = os.path.join(folder_path, 'spam_dataset.csv')
 
-# Membuat folder jika belum ada
+
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)
 
-# Menyimpan dataset ke file CSV di dalam folder yang ditentukan
 df.to_csv(file_path, index=False)
 
 print(f"Dataset telah disimpan di {file_path}")
